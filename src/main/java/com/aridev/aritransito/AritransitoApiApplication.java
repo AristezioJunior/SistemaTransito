6b71afa51d4fce865e8f2482e66c13633963b093
Aristezio Junior <aristezio@gmail.com>
2023-07-14 22:36:52 -0300

    Criando sistema de transito

---
+++ src/main/java/com/aridev/aritransito/AritransitoApiApplication.java
@@ -0,0 +1,13 @@
+package com.aridev.aritransito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AritransitoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AritransitoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aridev/aritransito/AritransitoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aridev.aritransito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AritransitoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
