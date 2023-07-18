aa19b50378e561919ca64f2432f9a416c28e5505
Aristezio Junior <aristezio@gmail.com>
2023-07-17 21:36:16 -0300

    implementando jakarta EE JPA

--- src/main/java/com/aridev/aritransito/domain/model/Proprietario.java
+++ src/main/java/com/aridev/aritransito/domain/model/Proprietario.java
@@ -1,16 +1,26 @@
 package com.aridev.aritransito.domain.model;
 
+import jakarta.persistence.*;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.springframework.boot.autoconfigure.web.WebProperties;
 
 @Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+//@Table(name = "tb_proprietario")
 public class Proprietario {
 
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String nome;
 
     private String email;
 
+    @Column(name = "fone")
     private String telefone;
 
 }
