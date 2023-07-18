fb4521046645304fcf6b9208869452d57d4c3409
Aristezio Junior <aristezio@gmail.com>
2023-07-18 10:00:03 -0300

    adicionando endpoint de buscar proprietario por id

--- src/main/java/com/aridev/aritransito/api/controller/ProprietarioController.java
+++ src/main/java/com/aridev/aritransito/api/controller/ProprietarioController.java
@@ -4,22 +4,35 @@ import com.aridev.aritransito.domain.model.Proprietario;
 import com.aridev.aritransito.domain.repository.ProprietarioRepository;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
 import java.util.List;
 
+@AllArgsConstructor
 @RestController
+@RequestMapping("/proprietarios")
 public class ProprietarioController {
 
     @Autowired
     private ProprietarioRepository proprietarioRepository;
 
-    @GetMapping("/proprietarios")
+    @GetMapping
     public List<Proprietario> listar() {
         return proprietarioRepository.findAll();
+    }
+
+    @GetMapping("/{proprietarioId}")
+    public ResponseEntity<Proprietario> buscar(@PathVariable Long proprietarioId) {
+        return proprietarioRepository.findById(proprietarioId)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
     }
 
 }
