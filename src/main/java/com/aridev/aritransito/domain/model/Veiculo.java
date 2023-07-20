27beff03c8b169d38079a8faecab63c6725beb68
Aristezio Junior <aristezio@gmail.com>
2023-07-20 14:50:20 -0300

    Alterando LocalDateTime para OffsetDateTime para respeitar padrao rest

--- src/main/java/com/aridev/aritransito/domain/model/Veiculo.java
+++ src/main/java/com/aridev/aritransito/domain/model/Veiculo.java
@@ -15,6 +15,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
 
 @Getter
 @Setter
@@ -49,7 +50,7 @@ public class Veiculo {
     private StatusVeiculo status;
 
     @JsonProperty(access = Access.READ_ONLY)
-    private LocalDateTime dataCadastro;
+    private OffsetDateTime dataCadastro;
 
     @JsonProperty(access = Access.READ_ONLY)
     private LocalDateTime dataApreensao;
