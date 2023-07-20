35d1482f8dbf04f5a1df0a039048d622a22907b2
Aristezio Junior <aristezio@gmail.com>
2023-07-20 16:29:26 -0300

    Implementando assembler de representation model

---
+++ src/main/java/com/aridev/aritransito/api/assembler/VeiculoAssembler.java
@@ -0,0 +1,27 @@
+package com.aridev.aritransito.api.assembler;
+
+import com.aridev.aritransito.api.model.VeiculoModel;
+import com.aridev.aritransito.domain.model.Veiculo;
+import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@AllArgsConstructor
+@Component
+public class VeiculoAssembler {
+
+    private final ModelMapper modelMapper;
+
+    public VeiculoModel toModel(Veiculo veiculo) {
+        return modelMapper.map(veiculo, VeiculoModel.class);
+    }
+
+    public List<VeiculoModel> toCollectionModel(List<Veiculo> veiculos) {
+        return veiculos.stream()
+                .map(this::toModel)
+                .toList();
+    }
+
+}
