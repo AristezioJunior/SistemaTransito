a4ac57385cc6aae4fbfcfb148b63d4937bd40649
Aristezio Junior <aristezio@gmail.com>
2023-07-20 09:03:48 -0300

    Implementando classe ApiExceptionHandler

--- src/main/java/com/aridev/aritransito/api/controller/VeiculoController.java
+++ src/main/java/com/aridev/aritransito/api/controller/VeiculoController.java
@@ -40,10 +40,4 @@ public class VeiculoController {
     }
 
 
-
-    @ExceptionHandler(NegocioException.class)
-    public ResponseEntity<String> capturar(NegocioException e) {
-        return ResponseEntity.badRequest().body(e.getMessage());
-    }
-
 }
---
+++ src/main/java/com/aridev/aritransito/api/exceptionhandler/ApiExceptionHandler.java
@@ -0,0 +1,16 @@
+package com.aridev.aritransito.api.exceptionhandler;
+
+import com.aridev.aritransito.domain.exception.NegocioException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ApiExceptionHandler {
+
+    @ExceptionHandler(NegocioException.class)
+    public ResponseEntity<String> capturar(NegocioException e) {
+        return ResponseEntity.badRequest().body(e.getMessage());
+    }
+
+}
